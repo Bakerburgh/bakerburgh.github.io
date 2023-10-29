9e28de603831ae9791c80abe0569e3f44c3bb92e
Bakerburgh <16124805+Bakerburgh@users.noreply.github.com>
2023-10-29 17:48:30 -0600

    Just ran npm create

--- src/apps/DominoCounter/SvgParts/DominoBackgroundFull.tsx
+++
@@ -1,8 +0,0 @@
-function DominoBackgroundFull() {
-  return <g>
-    <rect x="0" y="0" width="20" height="40" rx="3" fill="#FFF" stroke="#000" stroke-width="1"/>
-    <path d="M 1.5 20 L 18.5 20" stroke="#000" stroke-width="1.5"/>
-  </g>
-}
-
-export default DominoBackgroundFull;
